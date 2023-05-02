eea90abd9a3d4a5f401f5c738b57a86deb67783b
Sergey Chumak <for_serega@mail.ru>
2023-05-02 17:19:44 +0300

    feat: add blocks "news" and "contacts" to the main page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,3 +33,40 @@ const articlesSwiper = new Swiper('.articles .swiper', {
     prevEl: ".articles__navigation .swiper-button-prev"
   }
 });
+
+const newsSwiper = new Swiper('.news .swiper', {
+  modules: [Navigation],
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".news__navigation .swiper-button-next",
+    prevEl: ".news__navigation .swiper-button-prev"
+  }
+});
+
+
+ymaps.ready(init);
+let myMap;
+
+function init() {
+
+  myMap = new ymaps.Map("map", {
+    center: [43.238253, 76.945465], // Координаты центра карты
+    zoom: 13 // Маштаб карты
+  });
+
+  myMap.controls.add(
+    new ymaps.control.ZoomControl()  // Добавление элемента управления картой
+  );
+
+  myPlacemark = new ymaps.Placemark([43.238253, 76.945465], { // Координаты метки объекта
+    balloonContent: "<div class='ya_map'>Заезжайте в гости!</div>" // Подсказка метки
+  }, {
+    preset: "twirl#redDotIcon" // Тип метки
+  });
+
+  myMap.geoObjects.add(myPlacemark); // Добавление метки
+  myPlacemark.balloon.open(); // Открытие подсказки метки
+
+};
