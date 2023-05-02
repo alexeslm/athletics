73453db49d0afa116046cc4ef96dade20adac117
Sergey Chumak <for_serega@mail.ru>
2023-05-02 12:01:38 +0300

    feat: add the block "brands" to the main page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+import Swiper, {Navigation} from "swiper";
+import 'swiper/css';
+
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger_active');
@@ -7,4 +10,15 @@ document.body.addEventListener('click', (event) => {
   if (!event.target.closest('.hamburger') && event.target !== mobileMenu) {
     hamburger.classList.remove('hamburger_active');
   }
-})
+});
+
+const brandsSwiper = new Swiper('.brands .swiper', {
+  modules: [Navigation],
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 7,
+  navigation: {
+    nextEl: ".brands__navigation .swiper-button-next",
+    prevEl: ".brands__navigation .swiper-button-prev"
+  }
+});
